5677812fd0044da504d8ef703f065e9d4ceeb690
Roman Kovalchuk <deputatroma@gmail.com>
2021-05-07 03:35:58 +0300

    arhiv-8

--- react-js/homework/homework2/src/components/utilities/cart.js
+++ react-js/homework/homework2/src/components/utilities/cart.js
@@ -1,8 +1,8 @@
 function addToCart(itemId) {
 
-    const cart = JSON.parse(localStorage.getItem('cardList')) || {};
+    const cart = JSON.parse(localStorage.getItem('cartList')) || {};
     cart[itemId] = (cart[itemId] || 0) + 1;
-    localStorage.setItem('cardList', JSON.stringify(cart));
+    localStorage.setItem('cartList', JSON.stringify(cart));
 }
 
 export default addToCart;
